59d153259de9e8690b5be8a6fc22e33d14df3e32
rbenites <rbenites115@gmail.com>
2018-03-23 11:48:49 -0700

    added HTML info, buttons will dispaly alert

--- README.md
+++ README.md
@@ -20,5 +20,7 @@ If the player selects the correct answer, show a screen congratulating them for 
 The scenario is similar for wrong answers and time-outs.
 
 If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+
 If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+
 On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,86 @@
 /*jshint esversion: 6 */
 
 window.onload = function () {
+    //8 random questions with answers
+    var questions = {
+        question1: [{
+            question: "what color is the sky?",
+            guess: ["Green", "Red", "Orange", "Blue"],
+            answer: "Blue"
+        }]/*,
+        question2: [{
+            question: "",
+            guess: "",
+            answer: ""
+        }],
+        question3: [{
+            question: "",
+            guess: "",
+            answer: ""
+        }],
+        question4: [{
+            question: "",
+            guess: "",
+            answer: ""
+        }],
+        question5: [{
+            question: "",
+            guess: "",
+            answer: ""
+        }],
+        question6: [{
+            question: "",
+            guess: "",
+            answer: ""
+        }],
+        question7: [{
+            question: "",
+            guess: "",
+            answer: ""
+        }],
+        question8: [{
+            question: "",
+            guess: "",
+            answer: ""
+        }]*/
+    };
+    console.log(questions.question1[0].question);
+    for (i = 0; i < 4; i++) {
+        console.log(questions.question1[0].guess[i]);
+    }
+    console.log(questions.question1[0].answer);
+
+    //pupulate area with random questions
+    $(".lead").text(questions.question1[0].question);
+
+    //populate guesses to chose from
+    $("#1").text(questions.question1[0].guess[0]);
+    $("#2").text(questions.question1[0].guess[1]);
+    $("#3").text(questions.question1[0].guess[2]);
+    $("#4").text(questions.question1[0].guess[3]);
+
+    //need event listener for button click to track if the user clicks the correct button or not
+    $("#1,#2,#3,#4").on("click", function () {
+        //if users selects correct answer Alert they guessed correctly else alert incorrect and show correct answer
+        if (this.id == '4') {
+            alert("correct " + questions.question1[0].guess[3]);
+        } else {
+            alert("incorrect " + questions.question1[0].guess[0]);
+        }
+    });
+    //creat timer for the alert
+    //after ten seconds the alert is auto dismissed and the next question appears
+
+    //create a question timer
+        //if user guesses correctly dimiss te time, display the correct alert, reset timer for next question
+        //if user runs out of time alert that they ran out of time, 
+        //display the correct answer, 
+        //call alert timer
+
+    //once the final question hase been displayed and answered show the user their scrore
+
+    //show an option to restart the game through a button click that DOES NOT refresh the page
 
 
-    
+
 };
